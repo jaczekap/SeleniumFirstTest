6f19cf3ec0097674946d7d5ec44193f7efc89c20
User <User@DESKTOP-EIUQFPA>
2019-03-09 19:16:33 +0100

    added test for JavaScriptExecutor usage

---
+++ SeleniumFirstTest/src/test/java/test/JavaScriptExecutionTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptExecutionTest {
+	
+	private WebDriver driverChrome;
+	
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driverChrome = new ChromeDriver();
+		driverChrome.manage().window().maximize();
+		driverChrome.get("http://www.google.com");
+	}
+	
+	
+	@Test
+	public void testWindowTitleIsCorrect() {
+		JavascriptExecutor js = (JavascriptExecutor) driverChrome;
+		var title = js.executeScript("return document.title");
+		assertEquals("Google", title);
+	}
+	
+	@After
+	public void TearDown() {
+		driverChrome.close();
+	}
+
+}
