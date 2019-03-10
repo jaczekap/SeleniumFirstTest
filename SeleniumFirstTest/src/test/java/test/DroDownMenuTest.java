01ca7ab5ecd05ccd1c08a571f3f4cbda1d2c31e9
User <User@DESKTOP-EIUQFPA>
2019-03-10 13:32:53 +0100

    added new tests for dropdown menu

---
+++ SeleniumFirstTest/src/test/java/test/DroDownMenuTest.java
@@ -0,0 +1,60 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DroDownMenuTest {
+	
+	private WebDriver driverChrome;
+	
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driverChrome = new ChromeDriver();
+		driverChrome.manage().window().maximize();
+		driverChrome.get("https://magento.com/");
+	}
+	
+	@Test
+	public void testNumberOfAvailableLanguages() {
+		List<WebElement> elements = getLanguages();
+		
+		assertEquals(6, elements.size());
+	}
+
+	@Test
+	public void testAllLanguagesArePresent() {
+		List<String> expectedLanguages = new ArrayList<String>( Arrays.asList("", "Français (FR)", "Deutsch (DE)", "Português (BR)", "Español (LATAM)", "日本の (JP)"));
+		List<WebElement> elements = getLanguages();
+		List<String> actualLanguages = elements.stream().map(e -> e.getText()).collect(Collectors.toList());
+		
+		assertEquals(expectedLanguages, actualLanguages);
+	}
+	
+	
+	@After
+	public void TearDown() {
+		driverChrome.close();
+	}
+	
+	private List<WebElement> getLanguages() {
+		driverChrome.findElement(By.xpath("//*[@id=\"block-footerlanguages\"]/div/div")).click();
+		WebElement language = driverChrome.findElement(By.xpath("//*[@id=\"block-footerlanguages\"]/div/ul"));
+		
+		List<WebElement> elements = language.findElements(By.tagName("li"));
+		return elements;
+	}
+}
--- SeleniumFirstTest/src/test/java/test/JavaScriptExecutionTest.java
+++ SeleniumFirstTest/src/test/java/test/JavaScriptExecutionTest.java
@@ -3,12 +3,17 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class JavaScriptExecutionTest {
 	
@@ -19,7 +24,8 @@ public class JavaScriptExecutionTest {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 		driverChrome = new ChromeDriver();
 		driverChrome.manage().window().maximize();
-		driverChrome.get("http://www.google.com");
+		//driverChrome.get("http://www.google.com");
+		driverChrome.get("https://magento.com/");
 	}
 	
 	
